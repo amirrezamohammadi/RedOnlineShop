80407f15e46da1903f2a62735d801cfe7b906ba1
AliKawosi <alikawosi.work@gmail.com>
2023-08-19 13:58:05 +0100

    Email setup done.

---
+++ RedOnlineShop/Models/EmailSetting.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace RedOnlineShop.Models
+{
+	public partial class EmailSetting
+	{
+        public string Email { get; set; }
+        public string AppPass { get; set; }
+
+    }
+}
+
